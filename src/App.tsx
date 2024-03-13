1923c7326d49dbd48084f1f1328f72610ff66376
Victor Elgersma <victor.elgersma41@thehutgroup.com>
2024-03-13 16:21:34 +0000

    made fruit facts disappear on click

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import "./App.css";
 import FruitFact from "./components/FruitFact";
+import { useState } from "react";
 
 function App() {
-  const buttonText = "Show me fruit facts";
+  const [showFacts, setShowFacts] = useState(false);
+  const [buttonText, setButtonText] = useState("Show me fruit facts!");
+
   const fruitFactsArray = [
     { name: "Banana", fact: "I am yellow in color" },
     { name: "Peach", fact: "I have fuzzy skin" },
     { name: "Apple", fact: "I tend to be crunchy" },
   ];
+  const handleClick = () => {
+    setShowFacts((prev) => !prev);
+    setButtonText("Fruit Facts are boring!");
+  };
 
   return (
     <>
       <h2>Day 4 / Random Fruit Facts</h2>
-      <button>{buttonText}</button>
-      {fruitFactsArray.map(({ name, fact }) => (
-        <div key={name}>
-          <FruitFact fruit={name} fact={fact} />
-        </div>
-      ))}
+      <button onClick={handleClick}>{buttonText}</button>
+      {showFacts &&
+        fruitFactsArray.map(({ name, fact }) => (
+          <div key={name}>
+            <FruitFact fruit={name} fact={fact} />
+          </div>
+        ))}
     </>
   );
 }
