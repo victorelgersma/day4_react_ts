50c4a22c2fd6186b079dc49248983e14573352c5
Victor Elgersma <victor.elgersma41@thehutgroup.com>
2024-03-13 16:01:44 +0000

    added fruit facts array

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,12 @@ function App() {
     setDisplayFruitFacts(false);
   };
 
+  const fruitFactsArray = [
+    { name: "Banana", fact: "I am yellow in color" },
+    { name: "Peach", fact: "I have fuzzy skin" },
+    { name: "Apple", fact: "I tend to be crunchy" },
+  ];
+
   return (
     <>
       <h2>Day 4 / Random Fruit Facts</h2>
@@ -15,6 +21,9 @@ function App() {
         ComponentWillDisplayFruitFacts={displayFruitFacts}
         handleClick={updateText}
       ></FruitFacts>
+      {fruitFactsArray.map(() => {
+        <div></div>;
+      })}
     </>
   );
 }
